0f8b64fbad20485629cdb997e4c56ae0927a989c
TGunderson <tgunderson@gmail.com>
2025-06-28 18:17:35 -0700

    changed auth flow

--- caregiver-portal/src/main.jsx
+++ caregiver-portal/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from './auth/useAuth';
-import { AuthCallback } from './components/AuthCallback';
+
 import { useAuth } from './auth/useAuth';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
