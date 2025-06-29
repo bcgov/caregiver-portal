23894e98312eeadd1f63bf62d2b47e4f3fcef126
TGunderson <tgunderson@gmail.com>
2025-06-28 18:17:41 -0700

    removed unused library

--- caregiver-portal/src/main.jsx
+++ caregiver-portal/src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from './auth/useAuth';
 
-import { useAuth } from './auth/useAuth';
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
