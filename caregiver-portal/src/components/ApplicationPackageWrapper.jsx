4df0ea34cb19658df97ea130f46be7c6d0a41feb
TGunderson <tgunderson@gmail.com>
2025-08-20 09:38:20 -0700

    refactored naming for PascalCase and removed old files

--- caregiver-portal/src/components/ApplicationPackageWrapper.jsx
+++ caregiver-portal/src/components/ApplicationPackageWrapper.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import ApplicationPackage from './application-package';
+import ApplicationPackage from './ApplicationPackage';
 
 const ApplicationPackageWrapper = () => {
   const { applicationId } = useParams();
