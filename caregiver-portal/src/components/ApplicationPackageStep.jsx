4d25ed4060b8a096ef22f3d242271f4034bb94b6
TGunderson <tgunderson@gmail.com>
2025-10-20 16:04:58 -0700

    show completion state on icon

--- caregiver-portal/src/components/ApplicationPackageStep.jsx
+++ caregiver-portal/src/components/ApplicationPackageStep.jsx
@@ -3,12 +3,22 @@ import { ArrowRight, CircleAlert, CircleCheck, CircleArrowRight, ChevronRight } 
 import Button from "./Button";
 
 
-const ApplicationPackageStep = ({step, index, onContinue}) => {
+const ApplicationPackageStep = ({step, index, onContinue, state = 'default'}) => {
+
+    const getIcon = () => {
+        switch (state) {
+            case 'complete':
+                return <CircleCheck className="application-package-icon-complete" />;
+            case 'default':
+            default:
+                return <CircleAlert className="application-package-icon-alert" />;       
+        }
+    }
 
     return (
         <button className="application-package-step" key={index} onClick={onContinue}>
             <span className="application-package-left">
-                <CircleAlert className="application-package-icon-alert" />
+                {getIcon()}
                 <span className="application-package-label">{step.type}</span>
             </span>
             <ChevronRight className="application-package-chevron" />
