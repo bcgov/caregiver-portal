57b60810e9424711eea26194c9b64a1d9b46c8bb
TGunderson <tgunderson@gmail.com>
2025-11-07 19:42:53 -0800

    Updated Application Start forms

--- caregiver-portal/src/components/OOCApplicationStart.jsx
+++ caregiver-portal/src/components/OOCApplicationStart.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Button from './Button';
-import Family from '../assets/foster-hero2.png';
+import Family from '../assets/kinship-hero.png';
 import { ExternalLink } from "lucide-react";
 
-const FosterApplicationStart = ({onClick, disabled = false}) => {
+const OOCApplicationStart = ({onClick, disabled = false}) => {
     const [isStarting, setIsStarting] = React.useState(false);
 
     const handleStartClick = async () => {
@@ -20,20 +20,20 @@ const FosterApplicationStart = ({onClick, disabled = false}) => {
 
             <div className="page-details-frame">
                 <div className="image-frame">
-                    <img src={Family} alt="Become a foster caregiver" className="hero-image" />
+                    <img src={Family} alt="Explore Out-of-Care (kinship)" className="hero-image" />
                     <hr className="gold-underline-large" />
-                    <h2 className="page-heading">Foster caregiving</h2>
+                    <h2 className="page-heading">Out-of-Care (kinship)</h2>
                 </div>
-                <p className="page-content">Foster caregiving is about opening your home and sharing your love, nurturing and caring for children and youth in B.C. who are under the age of 19 and who temporarily cannot live with their own families.</p>
+                <p className="page-content">When a chid or youth is unable to live with their parents, it's best if they can live with someone they know.</p>
                 <div className="buttonGroup">
                     {!disabled && (
                     <Button onClick={handleStartClick} 
                       variant={isStarting ? "disabled" : "primary"}>Start application</Button>
                     )}
-                    <Button onClick={() => {window.location.href="https://www2.gov.bc.ca/gov/content/family-social-supports/fostering/caringforchildrenandyouth/fostercaregiving"}} variant="secondary">Learn more <ExternalLink className="buttonIcon" /></Button>
+                    <Button onClick={() => {window.location.href="https://www2.gov.bc.ca/gov/content/family-social-supports/fostering/caringforchildrenandyouth/kinshipcare"}} variant="secondary">Learn more <ExternalLink className="buttonIcon" /></Button>
                 </div>
             </div>
     )
 }
 
-export default FosterApplicationStart;
+export default OOCApplicationStart;
