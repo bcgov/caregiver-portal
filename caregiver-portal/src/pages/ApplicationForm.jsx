b17611086f960420018666b773c2e174b7359ff8
TGunderson <tgunderson@gmail.com>
2025-12-29 09:14:24 -0800

    handle navigation within breadcrumbs

--- caregiver-portal/src/pages/ApplicationForm.jsx
+++ caregiver-portal/src/pages/ApplicationForm.jsx
@@ -1,30 +1,17 @@
 import React from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import "../DesignTokens.css";
-import Breadcrumb from '../components/Breadcrumb';
+
 import Application from '../components/Application';
 
 
 const ApplicationForm = () => {
   const { applicationPackageId, applicationFormId } = useParams();
-  const navigate = useNavigate();
-  const back = `/foster-application/application-package/${applicationPackageId}`;
-
-  const breadcrumbItems = [
-    { label: 'Back', path: back},
-  ];
-
-  const handleBackClick = (item) => {
-    navigate(item.path);
-  };
 
   return (
     <div className="page">
       <div className="page-details">
-        <div className="page-details-row-breadcrumb">
-        <Breadcrumb items={breadcrumbItems} onBackClick={handleBackClick} />  
-        </div>
-        <Application applicationFormId={applicationFormId} onSubmitComplete={back} submitPackage={false}/>
+        <Application applicationPackageId={applicationPackageId} applicationFormId={applicationFormId} submitPackage={false}/>
     </div>
     </div>
   );
