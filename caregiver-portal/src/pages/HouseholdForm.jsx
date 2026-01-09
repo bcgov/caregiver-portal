436e6ba1c82c8ec1e71c157a7a2bdadd11d1c094
TGunderson <tgunderson@gmail.com>
2026-01-09 13:24:18 -0800

    made form feel similar to iframe with scrolling and breadcrumbs

--- caregiver-portal/src/pages/HouseholdForm.jsx
+++ caregiver-portal/src/pages/HouseholdForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import "../DesignTokens.css";
-import Breadcrumb from '../components/Breadcrumb';
+import BreadcrumbBar from '../components/BreadcrumbBar';
 import Household from '../components/Household';
 
 const HouseholdForm = () => {
@@ -16,22 +16,32 @@ const HouseholdForm = () => {
     navigate(item.path);
   };
 
+  const home = `/foster-application/application-package/${applicationPackageId}`;
+  const nextUrl = "DOESNOTWORK"
+
   return (
-    <div className="page">
-      <div className="page-details">
-        <div className="page-details-row-breadcrumb">
-          
-        <Breadcrumb items={breadcrumbItems} onBackClick={handleBackClick} />  
-        </div>
+    <div className="household-container">
+    {/* Top breadcrumb - aligned with page content */}
+    <div className="breadcrumb-top">
+      <div className="breadcrumb-top-content">
+        <BreadcrumbBar home={home} next={nextUrl} label={"My household"}/>
+      </div>
+    </div>
+
+    {/* Scrollable content area */}
+    <div className="household-content">
+      <div className="household-content-inner">
         <div className="page-details-row-small">
-        <h1 className="page-title">My household</h1>
+          <h1 className="page-title">My household</h1>
         </div>
-        
+
         <div className="page-details-row">
           <Household applicationPackageId={applicationPackageId} applicationFormId={applicationFormId} />
         </div>
-        </div>
-        </div>
+      </div>
+    </div>
+
+  </div>
     
   );
 
