d448aac408f2e63da5d6bd2afcc226ce81769e1e
TGunderson <tgunderson@gmail.com>
2026-05-28 15:31:32 -0700

    calculate back button

--- caregiver-portal/src/pages/ApplicationForm.jsx
+++ caregiver-portal/src/pages/ApplicationForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import "../DesignTokens.css";
 
 import Application from '../components/Application';
@@ -7,11 +7,16 @@ import Application from '../components/Application';
 
 const ApplicationForm = () => {
   const { applicationPackageId, applicationFormId } = useParams();
+  const location = useLocation();
+  const basePath = location.pathname.startsWith('/kinship-application')
+      ? `/kinship-application/application-package/${applicationPackageId}`
+      : `/foster-application/application-package/${applicationPackageId}`;
+
 
   return (
     <div className="page">
       <div className="page-details">
-        <Application applicationPackageId={applicationPackageId} applicationFormId={applicationFormId} submitPackage={false}/>
+        <Application applicationPackageId={applicationPackageId} applicationFormId={applicationFormId} submitPackage={false} basePath={basePath}/>
     </div>
     </div>
   );
