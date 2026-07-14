01b35ec7cedb49519cd06df5d33a8784433bf7ac
TGunderson <tgunderson@gmail.com>
2026-07-14 15:20:55 -0700

    show cancelled applications until next login

--- caregiver-portal/src/components/TaskCard.jsx
+++ caregiver-portal/src/components/TaskCard.jsx
@@ -18,62 +18,54 @@ const TaskCard = ({subtype = 'FCH', applicationPackage}) => {
         }
     };
 
-    const {srStage} = applicationPackage;
-    /*
+    const {srStage, status} = applicationPackage;
+    
     const getStatusInfo = () => {
       if (!applicationPackage){
         return {
-          text: 'Loading...',
+          title: 'Loading...',
+          click: false,
+          icon: '',
           className: 'task-card-status--loading'
         }
       }
 
-      const {status, srStage, referralstate} = applicationPackage;
-
-      if (referralstate === 'Requested' && srStage !== 'Application') {
+      if (status === 'Withdrawn') {
         return {
-          text: 'Information Session Requested',
-          className: 'task-card-status--requested'
-        }
+          title: `Become a ${label} caregiver`,
+          click: false, 
+        };
       }
 
-
-      if (srStage === 'Application') {
+      if (srStage === 'Completed') {
         return {
-          text: 'Complete Your Application',
-          className: 'task-card-status--submitted'
-        }
-      }
-
-      if (srStage === 'Screening') {
-        return {
-          text: 'Screening in Progress',
-          className: 'task-card-status--requested'
+          title: '',
+          click: false,
         }
       }
 
       return {
-        text: 'In progress',
-        className: ''
+        title: `Become a ${label} caregiver`,
+        click: true,
       };
 
     };
-  */
-    //const statusInfo = getStatusInfo();
 
+    const statusInfo = getStatusInfo();
+  
     return (
-        <div className="task-card" onClick={() => srStage !== 'Compleed' ? handleClick() : null}>
+        <div className="task-card" onClick={() => statusInfo.click ? handleClick() : null}>
         <div className="task-card-content">
-
-            {srStage !== 'Completed' && (
+          {srStage !== 'Completed' && status !== 'Withdrawn' && (
             <div className="task-card-title">Become a {label} caregiver</div>
-          )}    
-            {srStage === 'Completed' && ( 
-              <div className="task-card-text">
-              <CircleCheck size={50} className="success-icon"/> Approved {label} Caregiver
-              </div>
-              )}
-            {srStage !== 'Completed' && (<Button variant="primary">Continue<ArrowRight></ArrowRight></Button>)}
+          )}
+          {status === 'Withdrawn' && (
+            <div className="task-card-text--cancelled"><p>Your {label} caregiver application has been cancelled<br/><br/><small>This message will disappear on your next login.</small></p></div>
+          )}
+          {srStage === 'Completed' && (
+            <div className="task-card-text"><CircleCheck size={50} className="success-icon" />Approved {label} Caregiver</div>
+          )}
+            {statusInfo.click && (<Button variant="primary">Continue<ArrowRight></ArrowRight></Button>)}
         </div>
       </div>
     );
