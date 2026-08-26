e03a401a5a1cf3187a636b8fc7cb8d3fe87deaf6
TGunderson <tgunderson@gmail.com>
2026-08-26 09:51:09 -0700

    refactored screening task card

---
+++ caregiver-portal/src/components/cards/ScreeningTaskCard.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Button from '../Button';
+import { ArrowRight } from 'lucide-react';
+import GenericTaskCard from './GenericTaskCard';
+import { useNavigate } from 'react-router-dom';
+
+const ScreeningTaskCard = ({applicationFormSet, householdMembership}) => {
+    const navigate = useNavigate();
+
+    const householdMemberId = applicationFormSet?.[0]?.householdMemberId ?? householdMembership?.householdMemberId;
+    // Get latest submission date
+    const latestSubmittedAt = applicationFormSet
+    ?.filter(form => form.submittedAt)
+    .reduce((latest, form) => {
+      const currentDate = new Date(form.submittedAt);
+      return !latest || currentDate > new Date(latest) ? form.submittedAt : latest;
+    }, null);
+
+    const handleClick = () => {
+      if (householdMemberId) {
+        navigate(`/screening-package/${householdMemberId}`);
+        }
+    };
+
+    return (
+      <>
+        {householdMembership?.screeningInfoProvided ? (
+          <GenericTaskCard
+            title="Your household screening form was successfully submitted"
+            description={`Submitted on ${latestSubmittedAt}`}
+          />
+        ) : (
+          <GenericTaskCard
+            title="Complete your foster caregiver household screening"
+            buttonLabel="Continue"
+            onClick={handleClick}
+          />
+        )}
+      </>
+    );
+  };
+
+export default ScreeningTaskCard;
